76746314636b23ac0df10c65cb993561a91b1ed8
lluli <llulitrex@gmail.com>
2025-10-26 21:35:22 -0300

    VH

--- src/Paginas/Pedidos/EnderecosCadastrados.jsx
+++ src/Paginas/Pedidos/EnderecosCadastrados.jsx
@@ -1,5 +1,5 @@
-import React, {useEffect, useState} from 'react';
-import {useNavigate, useSearchParams} from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useSearchParams } from "react-router-dom";
 import Menu from '../../Componentes/Menu';
 import "../../css/Home.css";
 import "../../css/Mensagens.css";
@@ -13,7 +13,7 @@ import api from "../../services/authApi";
 import useAuth from "../../hooks/useAuth";
 import HamburgerComponent from '../../Componentes/Menu/Hamburger';
 import useMenuTipo from "../../hooks/useMenuTipo";
-import {Search, UserRoundPen, Star, Megaphone, KeyRound, ShoppingBag, ArrowLeft, Check, ShoppingCart, X} from "lucide-react";
+import { Search, UserRoundPen, Star, Megaphone, KeyRound, ShoppingBag, ArrowLeft, Check, ShoppingCart, X } from "lucide-react";
 import FundoHome from "../../Imagens/DetalheFundo.png";
 
 
@@ -23,7 +23,7 @@ import imgAnuncioCamiseta from "../../Imagens/Anuncio_Titulo_1.png";
 import ItemCarrinho from "../../Componentes/ItemCarrinho"
 
 const Configuracao = () => {
-    const {menuTipo, menuOpen, setMenuOpen} = useMenuTipo(false);
+    const { menuTipo, menuOpen, setMenuOpen } = useMenuTipo(false);
     const [mostrarMenu, setMostrarMenu] = useState(true);
     const navigate = useNavigate();
     const [conteudoAtual, setConteudoAtual] = useState('');
@@ -76,13 +76,13 @@ export default Configuracao;
 const AbaConfig = ({ }) => (
     <div id="divConfig">
         <div className='divCarrinhoAberto'>
-            <div className="TituloCarinho">
-                <h1 style={{ fontWeight: "bold" }}>Resumo da Compra</h1>
-            </div>
-            <div className="divValoresPesquisa">
-                <div>
-                    <h2>Produtos:</h2>
-                    <h2>R$ 75,00</h2>
+            <div id='TopTitulo'>
+                <div className="TituloCarinho">
+                    <h1 style={{ fontWeight: "bold" }}>Resumo da Compra</h1>
+                </div>
+                <div className="divValoresPesquisa">
+                    <h2 id='produtos'>Produtos:</h2>
+                    <h2 id='preco'>R$ 75,00</h2>
                 </div>
             </div>
             <div className="ItensCarrinho">
@@ -93,18 +93,37 @@ const AbaConfig = ({ }) => (
                 <ItemCarrinho imgAnuncio={imgAnuncioCamiseta} nomeProduto="Camisa Branca" nomeVendedor="Victor Hugo" preco={40} />
             </div>
             <div className="ResultadoCarrinho">
+
                 <div className="divMensagensPesquisa divConfigPesquisa">
+                    {/*
                     <div className="barraPesquisa">
                         <input type="text" placeholder="Digite o seu CEP" />
                         <Check className="iconeLupa" size={24} color="#efefef" />
                     </div>
+                    */}
                 </div>
-                <div className="divValoresPesquisa">
+                <div className="divValoresColumn">
+                    <div className='Frete'>
+                        <h2>Frete Total:</h2>
+                        <h2 className="bold">R$ 10,00</h2>
+                    </div>
+                    <div className='Frete'>
+                        <h2>Frete 1:</h2>
+                        <h2>R$ 10,00</h2>
+                    </div>
+                    <div className='Frete'>
+                        <h2>Frete 2:</h2>
+                        <h2>R$ 10,00</h2>
+                    </div>
+                </div>
+                <div className="divValores">
                     <h2>Total:</h2>
                     <h2 className="bold">R$ 106,00</h2>
                 </div>
             </div>
+            {/*
             <button className="btnComprarAgora">Comprar agora</button>
+            */}
         </div>
     </div>
 );
@@ -113,16 +132,28 @@ const AreaConfig = ({ setMostrarAbaConfig, setMostrarAreaConfig, setMostrarMenu 
     <div className="AreaConfig" >
         <>
             <div className="divTituloArea">
-                { window.innerWidth < 500 &&
+                {window.innerWidth < 500 &&
                     <ArrowLeft size={30}
                                strokeWidth={2.5}
                                onClick={() => {
                                    setMostrarAreaConfig(false);
                                    setMostrarAbaConfig(true);
-                                   setMostrarMenu(true)}
+                                   setMostrarMenu(true)
+                               }
                                }
                     />}
+                <div className="lines">
+                    <div className="line line1"></div>
+                    <div className="line line2"></div>
+                </div>
                 <h2 className="titulo">Endereço(s) Cadastrado(s)</h2>
+                <div className="Endereco"></div>
+                <div className="Endereco"></div>
+                <div className="BotoesEndereco">
+                    <button className="btnCancelar">Cancelar</button>
+                    <button className="btnProximo">Próximo</button>
+                </div>
+
             </div>
         </>
     </div>
